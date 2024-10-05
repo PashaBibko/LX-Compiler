b004591f11968dafc038371867756624564706d6
PashaBibko <156938226+PashaBibko@users.noreply.github.com>
2024-10-05 23:07:56 +0100

    Functions are fixed

--- test/build/test.lx.cpp
+++ test/build/test.lx.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+int add(int a, int b)
+{
+	return a + b;
+}
 int main()
 {
+	std::cout << "hello";
+	std::cout << add(1, 2);;
 	if (true)
 {
-	std::cout;
+	std::cout << "all is normal";
 }
 else if (false)
 {
-	std::cout;
+	std::cout << "things are a bit weird";
 }
 else
 {
-	std::cout;
+	std::cout << "YOU ARE IN THE MATRIX";
 }
 
 }
